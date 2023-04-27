f5298b4bff9a1108d8ebfbf4b4789e25664b9c9d
tationchen46 <tationchen@gmail.com>
2023-04-26 21:10:41 -0700

    Change

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
+			
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("sign-up.fxml"));
 			Scene scene = new Scene(root,600,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
